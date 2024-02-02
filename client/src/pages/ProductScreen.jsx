5df33c05665f76a08395e6efe11b1fc530de34d3
Alba Giraldo <agiraldomolina@gmail.com>
2024-02-01 21:10:49 -0500

    fetch products using axios

--- client/src/pages/ProductScreen.jsx
+++ client/src/pages/ProductScreen.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react'
 import { useParams, Link} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
-import products from '../products'
 import Rating from '../components/Rating'
+import axios from 'axios'
 
 export default function ProductScreen() {
+    const [product, setProduct] = useState({});
+
+
+
     const { id: productId } = useParams()
-    const product = products.find((p)=>p._id ===productId)
-    console.log(product)
+    
+    useEffect(()=>{
+        const fetchProdut = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+        fetchProdut();
+    },[productId])
 
   return (
     <>
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:5000',
-      '/uploads': 'http://localhost:5000',
+      '/api':'http://localhost:8000',
+      '/uploads': 'http://localhost:8000',
     }
   },
   plugins: [react()],
