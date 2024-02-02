10bdc2cfcf352f2271f8c21018368fae729a4099
Alba Giraldo <agiraldomolina@gmail.com>
2024-02-01 19:06:19 -0500

    create product details page

--- client/src/components/Rating.jsx
+++ client/src/components/Rating.jsx
@@ -8,21 +8,21 @@ export default function Rating({ value, text}) {
     console.log(value)
   return (
     <div className="rating">
-        <sapan>
+        <span>
             {value >=1 ? <FaStar/>: value >= 0.5? <FaStarHalfAlt/>:<FaRegStar/>}
-        </sapan>
-        <sapan>
+        </span>
+        <span>
             {value >=2 ? <FaStar/>: value >= 1.5? <FaStarHalfAlt/>:<FaRegStar/>}
-        </sapan>
-        <sapan>
+        </span>
+        <span>
             {value >=3 ? <FaStar/>: value >= 2.5? <FaStarHalfAlt/>:<FaRegStar/>}
-        </sapan>
-        <sapan>
+        </span>
+        <span>
             {value >=4 ? <FaStar/>: value >= 3.5? <FaStarHalfAlt/>:<FaRegStar/>}
-        </sapan>
-        <sapan>
+        </span>
+        <span>
             {value ===5 ? <FaStar/>: value >= 4.5? <FaStarHalfAlt/>:<FaRegStar/>}
-        </sapan>
+        </span>
         <span className="rating-text">
             {text && text}
         </span>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,11 +11,13 @@ import './assets/styles/index.css'
 import App from './App.jsx'
 import './index.css'
 import HomeScreen from './pages/HomeScreen.jsx'
+import ProductScreen from './pages/ProductScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen/>} />
+      <Route path='/product/:id' element={<ProductScreen/>} />
     </Route>
   )
 )
