e74080c919a1a14819c246170d0aa109d144ce13
Alba Giraldo <agiraldomolina@gmail.com>
2024-02-02 19:58:27 -0500

    custom error middleware

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
 import connectDB from './config/db.js';
+import productRoutes from './routes/product.routes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 const port=process.env.PORT || 5000;
@@ -14,14 +16,10 @@ app.get('/',(req,res)=>{
     res.send('API is working');
 });
 
-app.get('/api/products',(req,res)=>{
-    res.json(products);
-})
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find(product => product._id === req.params.id);
-    res.json(product);
-})
+app.use('/api/products', productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
