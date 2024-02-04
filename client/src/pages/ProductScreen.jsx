51cda55af1556ef80443fade2a5b669fc5cdc807
Alba Giraldo <agiraldomolina@gmail.com>
2024-02-04 18:23:37 -0500

    addToCart function,  qty and addToCartHandler

--- client/src/pages/ProductScreen.jsx
+++ client/src/pages/ProductScreen.jsx
@@ -1,16 +1,33 @@
-import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import {  Link} from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlices'
+import { addToCart } from '../slices/cartSlice'
 
 export default function ProductScreen() {
     const {id:productId} = useParams();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [qty, setQty] = useState(1);
+
+    const {
+        data: product,
+        isLoading,
+        error
+    } = useGetProductDetailsQuery(productId);
+    
     console.log(productId);
-    const {data: product, isLoading, error} = useGetProductDetailsQuery(productId);
     console.log(product);
+    //console.log([...Array(product.countInStock).keys()])
+    const addToCartHandler=() => {
+        dispatch(addToCart({...product, qty}));
+        navigate('/cart');
+    }
     
   return (
     <>
@@ -62,11 +79,33 @@ export default function ProductScreen() {
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
+                                {product.countInStock > 0 && (
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Qty</Col>
+                                            <Col>
+                                                <Form.Control
+                                                    as='select'
+                                                    value={qty}
+                                                    onChange={(e) => setQty(Number(e.target.value))}
+                                                >
+                                                    {[...Array(product.countInStock).keys()].map((el)=>(
+                                                        <option key={el+1} value={el+1}>
+                                                            {el+1}
+                                                        </option>
+                                                    ))}
+                                                </Form.Control>
+                                            </Col>
+                                        </Row>
+
+                                    </ListGroup.Item>
+                                )}
                             <ListGroup.Item>
                                 <Button
                                     className='btn-block'
                                     type='button'
                                     disabled={product.countInStock === 0}
+                                    onClick={addToCartHandler}
                                 >
                                     Add To Cart
                                 </Button>
--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils,js";
 
 const initialState = localStorage.getItem('cart')? JSON.parse(localStorage.getItem('cart')) : {cartItems:[]};
 
@@ -6,8 +7,22 @@ const cartSlice= createSlice({
     name: 'cart',
     initialState,
     reducers: {
-
+        addToCart:(state, action)=>{
+            const item = action.payload;
+            const existItem = state.cartItems.find((el) => el._id === item._id);
+            if(existItem){
+                state.carItems =state.cartItems.map((el) => el._id===existItem._id? item : el);
+            }else{
+                state.cartItems = [...state.cartItems, item];
+            }
+            return updateCart(state);         
+        }
     }
-})
+});
+
+export const {
+    addToCart
+} = cartSlice.actions;
+
 
 export default cartSlice.reducer;
