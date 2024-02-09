0e97415bbe734cea08046508021a4a39b7d73ce8
Alba Giraldo <agiraldomolina@gmail.com>
2024-02-09 15:10:08 -0500

    Checkout steps component

--- client/src/pages/ShippingScreen.jsx
+++ client/src/pages/ShippingScreen.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import {saveShippingAddress} from "../slices/cartSlice";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 export default function ShippingScreen() {
     const cart = useSelector((state) => state.cart);
@@ -26,6 +27,7 @@ export default function ShippingScreen() {
 
   return (
     <FormContainer>
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form
             onSubmit={submitHandler}
