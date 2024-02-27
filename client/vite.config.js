c418d5d0ab4d0e459b08a7266ff09e8e2a6e1f5e
Alba Giraldo <agiraldomolina@gmail.com>
2024-02-27 01:06:03 -0500

    more changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':'http://localhost:8000',
-      '/uploads': 'http://localhost:8000',
+      secure: false,
     }
   },
   plugins: [react()],
